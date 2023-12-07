4dbcb9d2212510f94b5b46a306397a6e4107cb91
cheng.wang <cheng.wang@jiduauto.com>
2023-12-07 20:26:52 +0800

    Update SampleApplication.kt
    增加NPE案例

--- app/src/main/java/com/panda912/bandage/sample/SampleApplication.kt
+++ app/src/main/java/com/panda912/bandage/sample/SampleApplication.kt
@@ -25,7 +25,7 @@ class SampleApplication : Application() {
 
     private fun createDynamicBandageData() {
         // click button 'crash 10 / 0'
-        val stack =
+        val stackOfArithmeticException =
             "at com.panda912.bandage.sample.MainActivity.onCreate\$lambda-0(MainActivity.kt:14)\n" +
                     "at com.panda912.bandage.sample.MainActivity.\$r8\$lambda\$DDlE6wDlIjCyOonj-NgrVOem29Q(Unknown Source:0)\n" +
                     "at com.panda912.bandage.sample.MainActivity\$\$ExternalSyntheticLambda0.onClick(Unknown Source:0)\n" +
@@ -33,12 +33,38 @@ class SampleApplication : Application() {
         val dataOfArithmeticException = DynamicBandageData(
             process = "all",
             exceptionMatch = DynamicBandageData.ExceptionMatch("java.lang.ArithmeticException", ""),
-            stacks = stack.lines().map { it.trim().replace(Regex("(at |\\([^\\)]*\\))"), "") },
+            stacks = stackOfArithmeticException.lines()
+                .map { it.trim().replace(Regex("(at |\\([^\\)]*\\))"), "") },
             causes = null,
             closeCurActivity = true,
             loadPatch = false,
         )
 
-        Bandage.addDynamicBandageData(listOf(dataOfArithmeticException))
+        val stackOfNullPointerException =
+            " at com.panda912.bandage.sample.MainActivity.onCreate\$lambda-1(MainActivity.kt:19)\n" +
+                    "        at com.panda912.bandage.sample.MainActivity.\$r8\$lambda\$1D6Egf26udScyLg1CMAP2WCQtC0(Unknown Source:0)\n" +
+                    "        at com.panda912.bandage.sample.MainActivity\$\$ExternalSyntheticLambda1.onClick(Unknown Source:2)\n" +
+                    "        at android.view.View.performClick(View.java:7753)"
+
+        val dataOfNullPointerException = DynamicBandageData(
+            process = "all",
+            exceptionMatch = DynamicBandageData.ExceptionMatch(
+                "java.lang.NullPointerException",
+                ""
+            ),
+            stacks = stackOfNullPointerException.lines()
+                .map { it.trim().replace(Regex("(at |\\([^\\)]*\\))"), "") },
+            causes = null,
+            closeCurActivity = false,
+            loadPatch = false,
+        )
+
+
+        val list =
+            arrayListOf<DynamicBandageData>(dataOfNullPointerException, dataOfArithmeticException)
+
+
+
+        Bandage.addDynamicBandageData(list)
     }
 }
